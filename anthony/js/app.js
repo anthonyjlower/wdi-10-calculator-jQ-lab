7060f368efb5859c65d7743f73a8a57321022c62
Anthony <anthonyjlower@gmail.com>
2017-12-21 11:07:43 -0600

    Created event listener for number buttons

--- anthony/js/app.js
+++ anthony/js/app.js
@@ -1,1 +1,27 @@
 console.log('working')
+
+/*	What I need
+*	
+I need a variable to hold what is being calculated
+I need screen to default to 0, show whatever button was clicked last
+And then show the calculations
+When each .num-button is clicked I need it to understand the number value and show it on the screen
+*
+*
+*
+*
+*/
+
+
+//Create event listeners for buttons that prints the number buttons' value
+
+$('.num-button').on('click',(e)=>{
+	buttonValue = e.currentTarget
+	$buttonValue = ($(buttonValue).text())
+	numValue = parseInt($buttonValue, 10)
+	console.log(numValue)
+})
+
+
+
+
